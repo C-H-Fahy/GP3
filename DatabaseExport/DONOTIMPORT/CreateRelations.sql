14e2f32778ac53083c87043683694658a55a23d5
CHFahy <ciaran@ciaranfahy.com>
2022-03-29 23:09:11 +0100

    Fixed db relations

--- DatabaseExport/DONOTIMPORT/CreateRelations.sql
+++ DatabaseExport/DONOTIMPORT/CreateRelations.sql
@@ -1,4 +1,5 @@
 use orders;
+SET foreign_key_checks = 1;
 CREATE TABLE Cinema(
     id int unsigned AUTO_INCREMENT,    
     name char(255) NOT NULL UNIQUE,
@@ -6,7 +7,8 @@ CREATE TABLE Cinema(
     address text(511), 
     manager char(255),
     PRIMARY KEY(id)
-    );
+    ) 
+ENGINE=InnoDB;
 CREATE TABLE Screen(
     cinema int unsigned, 
     screen int unsigned,
@@ -14,16 +16,19 @@ CREATE TABLE Screen(
     price int unsigned NOT NULL,
     FOREIGN KEY(cinema) REFERENCES Cinema(id),
     PRIMARY KEY(cinema, screen)
-    );
+    )
+ENGINE=InnoDB;
 CREATE TABLE Film(
     id int unsigned AUTO_INCREMENT,    
     released int, 
     title char(255) NOT NULL,
     director char(255),
     PRIMARY KEY(id)
-    );
+    )
+ENGINE=InnoDB;
 ALTER TABLE Film
-    ADD CONSTRAINT titledirector UNIQUE (title, director);
+    ADD CONSTRAINT titledirector UNIQUE (title, director)   
+;
 
 CREATE TABLE Performance(
     id int unsigned AUTO_INCREMENT,
@@ -32,25 +37,26 @@ CREATE TABLE Performance(
     film int unsigned,
     date date,
     time time,
-    PRIMARY KEY(id),
-    FOREIGN KEY (cinema) REFERENCES cinema(ID), 
-    FOREIGN KEY (film) REFERENCES Film(ID)
-    );
+    PRIMARY KEY(id)
+    )
+ENGINE=InnoDB;
 ALTER TABLE Performance 
-    ADD CONSTRAINT Performance UNIQUE (cinema, screen, date, time);
-
-ALTER TABLE Performance ADD FOREIGN KEY (cinema, screen) REFERENCES Screen(cinema, screen);
+    ADD CONSTRAINT Performance UNIQUE (cinema, screen, date, time),
+    ADD FOREIGN KEY (cinema) REFERENCES cinema(ID), 
+    ADD FOREIGN KEY (film) REFERENCES Film(ID),
+    ADD FOREIGN KEY (cinema, screen) REFERENCES Screen(cinema, screen);
 
 CREATE TABLE `member`(
-  ID int unsigned NOT NULL AUTO_INCREMENT,
-  title char(15) DEFAULT NULL,
-  name char(255) NOT NULL UNIQUE,
-  joined date DEFAULT NULL,
-  active char(31) DEFAULT 'Active',
-  role_type char(16) DEFAULT 'member',
-  password char(100) NOT NULL,
-  PRIMARY KEY (`ID`)
-);
+    ID int unsigned NOT NULL AUTO_INCREMENT,
+    title char(15) DEFAULT NULL,
+    name char(255) NOT NULL UNIQUE,
+    joined date DEFAULT NULL,
+    active char(31) DEFAULT 'Active',
+    role_type char(16) DEFAULT 'member',
+    password char(100) NOT NULL,
+    PRIMARY KEY (`ID`)
+    )
+ENGINE=InnoDB;
 
 
 CREATE TABLE Booking(
@@ -61,12 +67,13 @@ CREATE TABLE Booking(
     Primary Key(ID),
     FOREIGN KEY (member) REFERENCES Member(ID),
     FOREIGN KEY (performance) REFERENCES Performance(ID)
-    );
-
+    )
+ENGINE=InnoDB;
 CREATE TABLE entrylogs(
     id int unsigned AUTO_INCREMENT,
     date DATE NOT NULL,
     time TIME NOT NULL,
     Primary Key(ID)
-    );
+    )
+ENGINE=InnoDB;
 
