4430888ffa6f5dbc4e577567382def89c7ac48d9
CHFahy <ciaran@ciaranfahy.com>
2022-03-29 23:09:11 +0100

    Fixed db relations

--- DatabaseExport/Export.sql
+++ DatabaseExport/Export.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2022 at 11:30 PM
+-- Generation Time: Mar 30, 2022 at 12:08 AM
 -- Server version: 5.6.15-log
 -- PHP Version: 5.5.8
 
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `booking` (
   PRIMARY KEY (`id`),
   KEY `member` (`member`),
   KEY `performance` (`performance`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `booking`
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `cinema` (
   `manager` char(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `cinema`
@@ -84,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `entrylogs` (
   `date` date NOT NULL,
   `time` time NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -99,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `film` (
   `director` char(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `titledirector` (`title`,`director`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `film`
@@ -135,13 +135,14 @@ CREATE TABLE IF NOT EXISTS `member` (
   `password` char(100) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3336 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3337 ;
 
 --
 -- Dumping data for table `member`
 --
 
 INSERT INTO `member` (`ID`, `title`, `name`, `joined`, `active`, `role_type`, `password`) VALUES
+(1, NULL, 'admin-test', '2022-03-17', 'Active', 'manager', '$2y$10$yTI.pu90o0ssPFQAiM7AkuBcGxM/.Zbk4UkMXRJJzr6W8in6fUsDW'),
 (1111, 'Ms', 'Helen Miranda', '2017-12-21', 'Active', 'member', '$2y$10$ExSVfcy/cMkG8RAAamP4ke0.hxmBaPwxruE2WG3P33Usg18AUZMGW'),
 (1234, 'Mr', 'Jose Alves', '2017-12-27', 'Active', 'member', '$2y$10$M3oW5tL.MWD2g8ZpvNcdiucn27i2KPFg40PFch.xA4eQs//pBUiqy'),
 (1333, 'Dr', 'Vito Gelato', '2018-01-06', 'Lapsed', 'member', '$2y$10$YoPiRYIY5Yhv/sLdHKfDeu8J.RY4OEQCp7O7.sJvEMZGPk/kSSxdO'),
@@ -150,7 +151,6 @@ INSERT INTO `member` (`ID`, `title`, `name`, `joined`, `active`, `role_type`, `p
 (2111, 'Ms', 'Lindsay White', '2018-03-16', 'Cancelled', 'member', '$2y$10$Tls7pfUEOVJqlDzuGoeOcuc66AvWRoxbsXSYmxPNYUd3yrGJTS532'),
 (2121, 'Mr', 'David Wilkinson', '2018-03-18', 'Active', 'member', '$2y$10$MbxtBf6xTaCLhz2mcFQ0CeYTmRFGzb4pW.ztUkum6p0J1snGXUFRS'),
 (3333, 'Ms', 'Olenka Sama', '2018-12-12', 'Active', 'member', '$2y$10$uZi.OEtK/d2HpmlSWB2jPewebL0zu2DlQwJmbfwdn7NQwVxPeBZ52'),
-(1, NULL, 'admin-test', '2022-03-17', 'Active', 'manager', '$2y$10$yTI.pu90o0ssPFQAiM7AkuBcGxM/.Zbk4UkMXRJJzr6W8in6fUsDW'),
 (3334, NULL, 'usher-test', '2017-12-21', 'Active', 'usher', '$2y$10$eF7x2SuwqcFh/SuYSqPtVuwY0gzzA6.LVZxA.N/waCD596eRE6Q4C'),
 (3335, NULL, 'recept-test', '2017-12-21', 'Active', 'receptionist', '$2y$10$M5SltbZMN1kYelm6YVOU5Oe.ad40F/RdklIJvznriAk7q/kOLv.9W');
 
@@ -170,7 +170,7 @@ CREATE TABLE IF NOT EXISTS `performance` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `Performance` (`cinema`,`screen`,`date`,`time`),
   KEY `film` (`film`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `performance`
@@ -205,7 +205,7 @@ CREATE TABLE IF NOT EXISTS `screen` (
   `seats` int(10) unsigned NOT NULL,
   `price` int(10) unsigned NOT NULL,
   PRIMARY KEY (`cinema`,`screen`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `screen`
@@ -219,6 +219,31 @@ INSERT INTO `screen` (`cinema`, `screen`, `seats`, `price`) VALUES
 (3, 1, 46, 400),
 (3, 2, 66, 300),
 (4, 1, 6, 800);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`member`) REFERENCES `member` (`ID`),
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`performance`) REFERENCES `performance` (`id`);
+
+--
+-- Constraints for table `performance`
+--
+ALTER TABLE `performance`
+  ADD CONSTRAINT `performance_ibfk_1` FOREIGN KEY (`cinema`) REFERENCES `cinema` (`id`),
+  ADD CONSTRAINT `performance_ibfk_2` FOREIGN KEY (`film`) REFERENCES `film` (`id`),
+  ADD CONSTRAINT `performance_ibfk_3` FOREIGN KEY (`cinema`, `screen`) REFERENCES `screen` (`cinema`, `screen`);
+
+--
+-- Constraints for table `screen`
+--
+ALTER TABLE `screen`
+  ADD CONSTRAINT `screen_ibfk_1` FOREIGN KEY (`cinema`) REFERENCES `cinema` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
