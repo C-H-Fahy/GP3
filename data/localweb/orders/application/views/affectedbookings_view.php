2ff4715b3d7c788fea3a7790f9d50b4bf7ee4833
CHFahy <ciaran@ciaranfahy.com>
2022-03-31 23:35:41 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/C-H-Fahy/GP3""
    This reverts commit 2518c2a6d8e91a37ca82d8e9ef25bcf6d9b07e7e.

--- data/localweb/orders/application/views/affectedbookings_view.php
+++ data/localweb/orders/application/views/affectedbookings_view.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,13 +8,16 @@
 	<style>
 		/* (Task 3.1) Alex: Clear styling of elements in a given screen */
 		h1 { text-align: center; font-size: 2em; font-family: 'Open Sans Light', sans-serif; color: purple; font-weight: 500;}
-		.Table__Films { margin: 0 5% 0 5%; }
+		.Table__Perf { margin: auto; width: 50%;}
 		.dataTablesContainer { font-size: 1em; font-family: 'Open Sans', sans-serif; }
 		.DataTables_sort_wrapper { color: purple; }
 		div.dataTables_wrapper .ui-widget-header { background: hsl(220, 12%, 95%); border: 0px solid black; box-shadow:0 4px 6px 0 hsla(0,0%,0%,0.2);}
-		table.dataTable {box-shadow:0 4px 6px 0 hsla(0,0%,0%,0.2);}
+		table.dataTable {box-shadow:0 4px 6px 0 hsla(0,0%,0%,0.2);}		
 		p.p-centre { text-align: center; font-size: 1em; font-family: 'Open Sans', sans-serif; }
 	</style>
+	
+
+<body>
 <?php 
 foreach($css_files as $file): ?>
 	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
@@ -22,23 +28,26 @@ foreach($css_files as $file): ?>
 </head>
 <body>
 
-<?php
-if($_SESSION['role'] === 'member'){
-    echo "You do not have permission to view this page";
-    return;
-}
-
-if($_SESSION['role'] !== 'manager'){
-    echo "<script>$('.add_button').remove();</script>";
-}
-?>
-
-<h1>Films</h1>
+<h1>Performances</h1>
 <p class="p-centre">
-	This table shows the Films available to be scheduled into Performances.
+	This table shows all the cancelled bookings.
 </p>
-    <div class="Table__Films">
+
+
+<button onclick="location.href='<?php echo site_url('main/performance')?>'">Return to performances view</button>
+
+ 
+    <div class="Table__Perf">
 		<?php echo $output; ?>
     </div>
+<script>
+const btn = $('.add_button');
+btn.remove();
+
+$('tbody').children().each((i)=>{
+  const row = $('tbody').children().eq(i).children();
+  row.eq(3).html('');
+});
+</script>
 </body>
 </html>
