17f17dd8e5eed3599e23b3675170bce2b19a940b
CHFahy <ciaran@ciaranfahy.com>
2022-03-31 23:33:03 +0100

    Revert "Merge branch 'main' of https://github.com/C-H-Fahy/GP3"
    This reverts commit c51dd9dc96c42dad8d16f644db3b64ff89a10e9c, reversing
    changes made to b97fc32fb1be97f39901ec871ff6043bdb5c2721.

--- DatabaseExport/Export.sql
+++ DatabaseExport/Export.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2022 at 02:51 AM
+-- Generation Time: Mar 27, 2022 at 03:17 AM
 -- Server version: 5.6.15-log
 -- PHP Version: 5.5.8
 
@@ -27,14 +27,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `booking` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `member` int(10) unsigned DEFAULT NULL,
-  `performance` int(10) unsigned NOT NULL,
-  `seats` int(10) unsigned NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `member` int(11) DEFAULT NULL,
+  `performance` int(11) NOT NULL,
+  `seats` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `member` (`member`),
   KEY `performance` (`performance`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `booking`
@@ -54,14 +54,14 @@ INSERT INTO `booking` (`id`, `member`, `performance`, `seats`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `cinema` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(255) NOT NULL,
   `location` char(255) DEFAULT NULL,
   `address` text,
   `manager` char(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `cinema`
@@ -80,11 +80,11 @@ INSERT INTO `cinema` (`id`, `name`, `location`, `address`, `manager`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `entrylogs` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `time` time NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -93,13 +93,13 @@ CREATE TABLE IF NOT EXISTS `entrylogs` (
 --
 
 CREATE TABLE IF NOT EXISTS `film` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `released` int(11) DEFAULT NULL,
   `title` char(255) NOT NULL,
   `director` char(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `titledirector` (`title`,`director`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `film`
@@ -126,7 +126,7 @@ INSERT INTO `film` (`id`, `released`, `title`, `director`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `member` (
-  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `title` char(15) DEFAULT NULL,
   `name` char(255) NOT NULL,
   `joined` date DEFAULT NULL,
@@ -135,14 +135,13 @@ CREATE TABLE IF NOT EXISTS `member` (
   `password` char(100) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3337 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3336 ;
 
 --
 -- Dumping data for table `member`
 --
 
 INSERT INTO `member` (`ID`, `title`, `name`, `joined`, `active`, `role_type`, `password`) VALUES
-(1, NULL, 'admin-test', '2022-03-17', 'Active', 'manager', '$2y$10$yTI.pu90o0ssPFQAiM7AkuBcGxM/.Zbk4UkMXRJJzr6W8in6fUsDW'),
 (1111, 'Ms', 'Helen Miranda', '2017-12-21', 'Active', 'member', '$2y$10$ExSVfcy/cMkG8RAAamP4ke0.hxmBaPwxruE2WG3P33Usg18AUZMGW'),
 (1234, 'Mr', 'Jose Alves', '2017-12-27', 'Active', 'member', '$2y$10$M3oW5tL.MWD2g8ZpvNcdiucn27i2KPFg40PFch.xA4eQs//pBUiqy'),
 (1333, 'Dr', 'Vito Gelato', '2018-01-06', 'Lapsed', 'member', '$2y$10$YoPiRYIY5Yhv/sLdHKfDeu8J.RY4OEQCp7O7.sJvEMZGPk/kSSxdO'),
@@ -151,6 +150,7 @@ INSERT INTO `member` (`ID`, `title`, `name`, `joined`, `active`, `role_type`, `p
 (2111, 'Ms', 'Lindsay White', '2018-03-16', 'Cancelled', 'member', '$2y$10$Tls7pfUEOVJqlDzuGoeOcuc66AvWRoxbsXSYmxPNYUd3yrGJTS532'),
 (2121, 'Mr', 'David Wilkinson', '2018-03-18', 'Active', 'member', '$2y$10$MbxtBf6xTaCLhz2mcFQ0CeYTmRFGzb4pW.ztUkum6p0J1snGXUFRS'),
 (3333, 'Ms', 'Olenka Sama', '2018-12-12', 'Active', 'member', '$2y$10$uZi.OEtK/d2HpmlSWB2jPewebL0zu2DlQwJmbfwdn7NQwVxPeBZ52'),
+(1, NULL, 'admin-test', '2022-03-17', 'Active', 'manager', '$2y$10$yTI.pu90o0ssPFQAiM7AkuBcGxM/.Zbk4UkMXRJJzr6W8in6fUsDW'),
 (3334, NULL, 'usher-test', '2017-12-21', 'Active', 'usher', '$2y$10$eF7x2SuwqcFh/SuYSqPtVuwY0gzzA6.LVZxA.N/waCD596eRE6Q4C'),
 (3335, NULL, 'recept-test', '2017-12-21', 'Active', 'receptionist', '$2y$10$M5SltbZMN1kYelm6YVOU5Oe.ad40F/RdklIJvznriAk7q/kOLv.9W');
 
@@ -161,16 +161,16 @@ INSERT INTO `member` (`ID`, `title`, `name`, `joined`, `active`, `role_type`, `p
 --
 
 CREATE TABLE IF NOT EXISTS `performance` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `cinema` int(10) unsigned NOT NULL,
-  `screen` int(10) unsigned NOT NULL,
-  `film` int(10) unsigned DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cinema` int(11) NOT NULL,
+  `screen` int(11) NOT NULL,
+  `film` int(11) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `time` time DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `Performance` (`cinema`,`screen`,`date`,`time`),
-  KEY `film` (`film`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+  KEY `film` (`film`),
+  KEY `cinema` (`cinema`,`screen`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `performance`
@@ -200,12 +200,12 @@ INSERT INTO `performance` (`id`, `cinema`, `screen`, `film`, `date`, `time`) VAL
 --
 
 CREATE TABLE IF NOT EXISTS `screen` (
-  `cinema` int(10) unsigned NOT NULL DEFAULT '0',
-  `screen` int(10) unsigned NOT NULL DEFAULT '0',
-  `seats` int(10) unsigned NOT NULL,
-  `price` int(10) unsigned NOT NULL,
+  `cinema` int(11) NOT NULL DEFAULT '0',
+  `screen` int(11) NOT NULL DEFAULT '0',
+  `seats` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
   PRIMARY KEY (`cinema`,`screen`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `screen`
@@ -219,31 +219,6 @@ INSERT INTO `screen` (`cinema`, `screen`, `seats`, `price`) VALUES
 (3, 1, 46, 400),
 (3, 2, 66, 300),
 (4, 1, 6, 800);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `booking`
---
-ALTER TABLE `booking`
-  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`member`) REFERENCES `member` (`ID`),
-  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`performance`) REFERENCES `performance` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `performance`
---
-ALTER TABLE `performance`
-  ADD CONSTRAINT `performance_ibfk_1` FOREIGN KEY (`cinema`) REFERENCES `cinema` (`id`),
-  ADD CONSTRAINT `performance_ibfk_2` FOREIGN KEY (`film`) REFERENCES `film` (`id`),
-  ADD CONSTRAINT `performance_ibfk_3` FOREIGN KEY (`cinema`, `screen`) REFERENCES `screen` (`cinema`, `screen`);
-
---
--- Constraints for table `screen`
---
-ALTER TABLE `screen`
-  ADD CONSTRAINT `screen_ibfk_1` FOREIGN KEY (`cinema`) REFERENCES `cinema` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- data/localweb/orders/application/views/items_view.php
+++ data/localweb/orders/application/views/items_view.php
@@ -3,14 +3,12 @@
 <head>
 	<meta charset="utf-8" />
 	<style>
-		/* (Task 3.1) Alex: Clear styling of elements in a given screen */
 		h1 { text-align: center; font-size: 2em; font-family: 'Open Sans Light', sans-serif; color: purple; font-weight: 500;}
 		.Table__Screens { margin: auto; width: 50%;}
-		.dataTablesContainer { font-size: 1em; font-family: 'Open Sans', sans-serif; }
+		.dataTablesContainer { font-size: 0.75em; font-family: 'Open Sans', sans-serif; color: purple; }
 		.DataTables_sort_wrapper { color: purple; }
 		div.dataTables_wrapper .ui-widget-header { background: hsl(220, 12%, 95%); border: 0px solid black; box-shadow:0 4px 6px 0 hsla(0,0%,0%,0.2);}
-		table.dataTable {box-shadow:0 4px 6px 0 hsla(0,0%,0%,0.2);}
-		p.p-centre { text-align: center; font-size: 1em; font-family: 'Open Sans', sans-serif; }	
+		table.dataTable {box-shadow:0 4px 6px 0 hsla(0,0%,0%,0.2);}	
 	</style>
 <?php 
 foreach($css_files as $file): ?>
@@ -21,16 +19,8 @@ foreach($css_files as $file): ?>
 <?php endforeach; ?>
 </head>
 <body>
-<?php
-if($_SESSION['role'] === 'member'){
-    echo "You do not have permission to view this page";
-    return;
-}
-?>
-<h1>Screens</h1>
-<p class="p-centre">
-	This table displays the different Screens available at Cinemas stored on the system.
-</p>
+
+<h1>Screen</h1>
     <div class="Table__Screens">
 		<?php echo $output; ?>
     </div>
