7c512f6867a2dbe925d788e5a5e84aa9fdda32c0
CHFahy <ciaran@ciaranfahy.com>
2022-03-31 23:33:03 +0100

    Revert "Merge branch 'main' of https://github.com/C-H-Fahy/GP3"
    This reverts commit 2287d58554ea54d596722f64bbec860e5ebd0fea, reversing
    changes made to b8bb8e397dc199ea48a12350ae943d79458e93e8.

--- DatabaseExport/DONOTIMPORT/CreateRelations.sql
+++ DatabaseExport/DONOTIMPORT/CreateRelations.sql
@@ -1,80 +1,69 @@
 use orders;
-SET foreign_key_checks = 1;
 CREATE TABLE Cinema(
-    id int unsigned AUTO_INCREMENT,    
+    id int AUTO_INCREMENT,    
     name char(255) NOT NULL UNIQUE,
     location char(255), 
     address text(511), 
     manager char(255),
     PRIMARY KEY(id)
-    ) 
-ENGINE=InnoDB;
+    );
 CREATE TABLE Screen(
-    cinema int unsigned, 
-    screen int unsigned,
-    seats int unsigned NOT NULL,
-    price int unsigned NOT NULL,
+    cinema int, 
+    screen int,
+    seats int NOT NULL,
+    price int NOT NULL,
     FOREIGN KEY(cinema) REFERENCES Cinema(id),
     PRIMARY KEY(cinema, screen)
-    )
-ENGINE=InnoDB;
+    );
 CREATE TABLE Film(
-    id int unsigned AUTO_INCREMENT,    
+    id int AUTO_INCREMENT,    
     released int, 
     title char(255) NOT NULL,
     director char(255),
     PRIMARY KEY(id)
-    )
-ENGINE=InnoDB;
+    );
 ALTER TABLE Film
-    ADD CONSTRAINT titledirector UNIQUE (title, director)   
-;
+    ADD CONSTRAINT titledirector UNIQUE (title, director);
 
 CREATE TABLE Performance(
-    id int unsigned AUTO_INCREMENT,
-    cinema int unsigned NOT NULL,
-    screen int unsigned NOT NULL,
-    film int unsigned,
+    id int AUTO_INCREMENT,
+    cinema int NOT NULL,
+    screen int NOT NULL,
+    film int,
     date date,
     time time,
     PRIMARY KEY(id),
     FOREIGN KEY (cinema) REFERENCES cinema(ID), 
-    FOREIGN KEY (film) REFERENCES Film(ID),
-    FOREIGN KEY (cinema, screen) REFERENCES Screen(cinema, screen)
-    )
-ENGINE=InnoDB;
-ALTER TABLE Performance 
-    ADD CONSTRAINT Performance UNIQUE (cinema, screen, date, time);
+    FOREIGN KEY (film) REFERENCES Film(ID)
+    );
+ALTER TABLE Performance ADD FOREIGN KEY (cinema, screen) REFERENCES Screen(cinema, screen);
 
-CREATE TABLE Member(
-    ID int unsigned NOT NULL AUTO_INCREMENT,
-    title char(15),
-    name char(255) NOT NULL UNIQUE,
-    joined date DEFAULT NULL,
-    active char(31) DEFAULT 'Active',
-    role_type char(16) DEFAULT 'member',
-    password char(100) NOT NULL,
-    PRIMARY KEY (`ID`)
-    )
-ENGINE=InnoDB;
+CREATE TABLE `member`(
+  ID int(11) NOT NULL AUTO_INCREMENT,
+  title char(15) DEFAULT NULL,
+  name char(255) NOT NULL UNIQUE,
+  joined date DEFAULT NULL,
+  active char(31) DEFAULT NULL DEFAULT 'Active',
+  role_type char(16) DEFAULT 'member',
+  password char(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+);
 
 
 CREATE TABLE Booking(
-    id int unsigned AUTO_INCREMENT,
-    member int unsigned,
-    performance int unsigned NOT NULL,
-    seats int unsigned NOT NULL,
+    id int AUTO_INCREMENT,
+    member int,
+    performance int NOT NULL,
+    seats int NOT NULL,
     Primary Key(ID),
     FOREIGN KEY (member) REFERENCES Member(ID),
-    FOREIGN KEY (performance) REFERENCES Performance(ID) ON DELETE CASCADE
-    )
-ENGINE=InnoDB;
+    FOREIGN KEY (performance) REFERENCES Performance(ID)
+    );
 
 CREATE TABLE entrylogs(
-    id int unsigned AUTO_INCREMENT,
+    id int AUTO_INCREMENT,
     date DATE NOT NULL,
     time TIME NOT NULL,
     Primary Key(ID)
-    )
-ENGINE=InnoDB;
+    );
 
--- data/localweb/orders/application/views/member_view.php
+++ data/localweb/orders/application/views/member_view.php
@@ -5,7 +5,7 @@
 	<style>
 		/* (Task 3.1) Alex: Clear styling of elements in a given screen */
 		h1 { text-align: center; font-size: 2em; font-family: 'Open Sans Light', sans-serif; color: purple; font-weight: 500;}
-		.Table__Members { margin: 0 5% 0 5%; }
+		.Table__Members { margin: auto; width: 75%;}
 		.dataTablesContainer { font-size: 1em; font-family: 'Open Sans', sans-serif; }
 		.DataTables_sort_wrapper { color: purple; }
 		div.dataTables_wrapper .ui-widget-header { background: hsl(220, 12%, 95%); border: 0px solid black; box-shadow:0 4px 6px 0 hsla(0,0%,0%,0.2);}
@@ -26,7 +26,6 @@ if($_SESSION['role'] === 'member' || $_SESSION['role'] === 'usher'){
     echo "You do not have permission to view this page";
     return;
 }
-
 ?>
 
 <h1>Members</h1>
@@ -35,37 +34,6 @@ if($_SESSION['role'] === 'member' || $_SESSION['role'] === 'usher'){
 	</p>
 	<div class="Table__Members">
 		<?php echo $output; ?>
-<script>
-const btn = $('.add_button');
-const link = btn.attr('href');
-const tmp = window.location.href.split('/');
-const CURRENT_PAGE = tmp[tmp.length-1];
-const x = link.split(CURRENT_PAGE+'/add');
-btn.attr('href', x[0] + 'register');
-
-$('tbody').children().each((i)=>{
-  const row = $('tbody').children().eq(i).children();
-  const uid = row.eq(0).html();
-  const stat = row.eq(4).html();
-  
-  const onclickBook = stat !== 'Active' ? `onclick="alert('Member does not have an active account.')"` : `href="${x[0]}performance?uid=${uid}"`;
-
-  row.eq(6).prepend(
-	`<a href="${x[0]}userbooking?uid=${uid}" class="edit_button ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary" role="button">
-		<span class="ui-button-icon-primary ui-icon ui-icon-document"></span>
-		<span class="ui-button-text">&nbsp;View Bookings</span>
-	</a>`);
-
-  row.eq(6).prepend(
-	`<a ${onclickBook} class="edit_button ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary" role="button">
-		<span class="ui-button-icon-primary ui-icon ui-icon-document"></span>
-		<span class="ui-button-text">&nbsp;Book</span>
-	</a>`);
-
-});
-</script>
-
-    </div>
+	</div>
 </body>
-
 </html>
